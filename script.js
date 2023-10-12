58724997ebf5e7cb783499b24068907bad27ec1c
indrawara017 <127364646+indrawara017@users.noreply.github.com>
2023-10-12 13:40:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+       var typed = new Typed(".multiple-text", {
+                strings: ["Informatics", "Gamer", "Programer"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            backDelay: 100,
+            loop: true
+   });
